859961dc4b70b9c63a5bbb87ace1c1a1e79aaed3
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-09-09 23:45:54 +0530

    QuestionController  Added

--- src/main/java/com/quiz/controller/QuestionsController.java
+++ src/main/java/com/quiz/controller/QuestionsController.java
@@ -1,4 +1,15 @@
 package com.quiz.controller;
 
-public class QuestionsController {
+import com.quiz.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionsController
+{
+    @Autowired
+    private QuestionService questionService;
+
 }
